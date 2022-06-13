23b5a92d0ac2d4ccba643f74f5223cfbd4afd760
Тихон Андреев <tihon.andreew@yandex.ru>
2022-06-13 23:20:20 +0200

    Add project files.

---
+++ LINQ/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int count = 0;
+int max;
+int[] a = new int[14];
+Random random = new Random();
+for (int i = 0; i < 14; i++)
+{
+    a[i] = random.Next(10, 100);
+    Console.Write(a[i]+"");
+    if (a[i] % 2 == 0)
+    {
+        count++;
+    }
+    Console.WriteLine("    " + count);
+}
+Console.WriteLine("\n");
+
+IEnumerable<int> b = from c in a
+                     where c % 2 == 0
+                     select c;
+
+foreach (int i in b)
+{
+    Console.Write(count + "  \n");
+}
